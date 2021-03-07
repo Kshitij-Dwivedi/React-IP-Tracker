53758b25fac6ece9035fa47367c34c12b17eae5b
Shantanu Tripathi <shaantanutripathi@gmail.com>
2021-03-07 19:09:35 +0530

    functions name changed and print long and lat

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
-import Input from './components/Input'
-const App = () => {
-  const skey="cad82a0c291128837a63b751f2f03664"
-  const trackip= async (add) =>{
-    const httpreq= "http://api.ipstack.com/"+add.address+'?access_key='+skey
-    const res= await fetch(httpreq)
-    const data= await res.json()
-    console.log(data)
-  }
-  return (
-    <div className='App'>
-      <h1>Hello Everyone</h1>
-      <Input ipset={trackip}/>
-    </div>
-  )
-}
+import Input from "./components/Input";
+import { useState } from "react";
 
-export default App
+const App = () => {
+    const [Location, setLocation] = useState([]);
+
+    const APIKEY = "cad82a0c291128837a63b751f2f03664";
+    const baseURL = "http://api.ipstack.com/";
+    const trackIP = async (IP) => {
+
+        const url = baseURL + IP + "?access_key=" + APIKEY;
+        const res = await fetch(url);
+        const data = await res.json();
+
+        console.log(data)
+        setLocation([data]);
+        console.log(Location[0].longitude);
+        console.log(Location[0].latitude);
+    };
+
+    return (
+        <div className="App">
+            <h1>Hello Everyone</h1>
+            <Input trackIP={trackIP} />
+        </div>
+    );
+};
+
+export default App;
