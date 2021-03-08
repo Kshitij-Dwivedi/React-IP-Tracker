0833c8abf5c28364e6947b88e37eff93ff13d9c4
Shantanu Tripathi <shaantanutripathi@gmail.com>
2021-03-08 20:26:02 +0530

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,23 +1,39 @@
-import {useState} from 'react'
+import { useState } from "react";
+import { Container, Button, Form } from "react-bootstrap";
 
-const Input = ({trackIP}) => {
-    const [Address, setAddress] = useState('')
-    const onSubmit= (e) =>{
-        e.preventDefault()
-        if(!Address)
-        {
-            alert('Please enter Ip Address')
-            return 
+const Input = ({ trackIP }) => {
+    const [Address, setAddress] = useState("");
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        if (!Address) {
+            alert("Please enter Ip Address");
+            console.log(Address);
+            return;
         }
-        trackIP(Address)
-        setAddress('')
-    }
+        trackIP(Address);
+        setAddress("");
+    };
     return (
-        <form className = "ip-form" onSubmit={onSubmit}>
-            <input type='text' placeholder='Ip Address' value={Address} onChange={(e) =>setAddress(e.target.value)}/>   
-            <input type='submit' value='Search'/>
-        </form>
-    )
-}
+        <Container
+            className="justify-content-center themed-container d-flex flex-column"
+            fluid="md"
+        >
+            <Form className="ip-form form-group" onSubmit={onSubmit}>
+                <Form.Group>
+                    <Form.Control
+                        type="text"
+                        placeholder="Search for any IP address or domain"
+                        value={Address}
+                        onChange={(e) => setAddress(e.target.value)}
+                    />
+                </Form.Group>
+                <Button variant="danger" type="submit">
+                    Submit
+                </Button>
+            </Form>
+        </Container>
+    );
+};
 
 export default Input;
