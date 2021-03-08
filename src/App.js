1892c84d40e0fdc5ec5d75af4ffdf9c54f3e963e
Shantanu Tripathi <shaantanutripathi@gmail.com>
2021-03-08 20:24:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Input from "./components/Input";
 import { useState } from "react";
-import MapContainer from './components/MapContainer'
+import MapContainer from "./components/MapContainer";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-    const [Location, setLocation] = useState([{latitude: 88.9, longitude: 87.6}]);
-    const [Ip, setIp] = useState("IP");
+    const [Location, setLocation] = useState([
+        { latitude: 88.9, longitude: 87.6 },
+    ]);
+    const [Ip, setIp] = useState("");
 
     const APIKEY = "cad82a0c291128837a63b751f2f03664";
     const baseURL = "http://api.ipstack.com/";
@@ -15,20 +19,22 @@ const App = () => {
         setIp(IP);
         setLocation([data]);
         console.log(Location);
-        // console.log(Location[0].longitude);
-        // console.log(Location[0].latitude);
     };
 
     return (
         <div className="App">
-            <h1>Hello Everyone</h1>
+            <h1 className = "head">IP Address Tracker</h1>
             <Input trackIP={trackIP} />
-            <h1>Location for {Ip} is:</h1>
-            <h1>
-                {Location[0].city}, {Location[0].country_name},{" "}
-                {Location[0].continent_name}
-            </h1>
-            <MapContainer latitude = {Location[0].latitude} longitude = {Location[0].longitude}/>
+            {Ip ? <h2 >Location for {Ip} is:</h2> : <h1></h1>}
+            {Ip ? (
+                <h2 className = "loc">
+                    {Location[0].city}, {Location[0].country_name},{" "}
+                    {Location[0].continent_name}
+                </h2>
+            ) : (
+                <h1></h1>
+            )}
+            {/* <MapContainer latitude = {Location[0].latitude} longitude = {Location[0].longitude}/> */}
         </div>
     );
 };
