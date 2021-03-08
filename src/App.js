3f1cbf74e08bcb2d73d43e73bb14f2aa031f48be
Shantanu Tripathi <shaantanutripathi@gmail.com>
2021-03-08 13:20:39 +0530

    Map Component in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import Input from "./components/Input";
 import { useState } from "react";
+import MapContainer from './components/MapContainer'
 
 const App = () => {
-    const [Location, setLocation] = useState([]);
+    const [Location, setLocation] = useState([{latitude: 88.9, longitude: 87.6}]);
+    const [Ip, setIp] = useState("IP");
 
     const APIKEY = "cad82a0c291128837a63b751f2f03664";
     const baseURL = "http://api.ipstack.com/";
     const trackIP = async (IP) => {
-
         const url = baseURL + IP + "?access_key=" + APIKEY;
         const res = await fetch(url);
         const data = await res.json();
-
-        console.log(data)
+        setIp(IP);
         setLocation([data]);
-        console.log(Location[0].longitude);
-        console.log(Location[0].latitude);
+        console.log(Location);
+        // console.log(Location[0].longitude);
+        // console.log(Location[0].latitude);
     };
 
     return (
         <div className="App">
             <h1>Hello Everyone</h1>
             <Input trackIP={trackIP} />
+            <h1>Location for {Ip} is:</h1>
+            <h1>
+                {Location[0].city}, {Location[0].country_name},{" "}
+                {Location[0].continent_name}
+            </h1>
+            <MapContainer latitude = {Location[0].latitude} longitude = {Location[0].longitude}/>
         </div>
     );
 };
